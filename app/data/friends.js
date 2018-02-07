12f7f58fd353ee8b6320ec9e3c782f691b1862a5
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-02-07 14:40:34 -0600

    draft

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+
+// https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array
+var allFriends = [
+    {
+        "name": "Joseph",
+        "photo": "test.jpg",
+        "scores": [
+            5,
+            3,
+            2,
+            5,
+            4,
+            3,
+            3,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Jacob",
+        "photo": "test2.jpg",
+        "scores": [
+            4,
+            1,
+            5,
+            2,
+            3,
+            3,
+            5,
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+      }
+]
+module.export = allFriends;
